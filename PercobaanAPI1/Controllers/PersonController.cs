2dd6065760658ca8a58a22d4b8ac6785094a1d4d
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2024-03-08 18:06:14 +0700

    Upload file

---
+++ PercobaanAPI1/Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+
+namespace PercobaanAPI1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext();
+            List<Person> ListPerson = context.ListPerson(); return Ok(ListPerson);
+        }
+
+        [HttpGet("api/person/{id}")]
+        public IActionResult GetPerson(int id)
+        {
+            PersonContext context = new PersonContext();
+            Person idPerson = context.GetPerson(id);
+
+            if (idPerson != null)
+            {
+                return Ok(idPerson);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+
+    }
+}
---
+++ PercobaanAPI1/models/PersonContex.cs
@@ -0,0 +1,31 @@
+namespace PercobaanApi1.Models
+{
+    public class PersonContext
+    {
+        public List<Person> ListPerson()
+        {
+            List<Person> list1 = new List<Person>(); Object[,] arrayPerson = new Object[,]{
+{1, "Budi", "Jember", "budi@gmail.com" },
+{2, "Iwan", "Banyuwangi", "iwan@gmail.com" },
+{3, "Wati", "Lumajang", "wati@gmail.com" }
+};
+            for (int i = 0; i < arrayPerson.GetLength(0); i++)
+            {
+                list1.Add(new Person()
+                {
+                    id_person = int.Parse(arrayPerson[i, 0].ToString()),
+                    nama = arrayPerson[i, 1].ToString(),
+                    alamat = arrayPerson[i, 2].ToString(),
+                    email = arrayPerson[i, 3].ToString()
+                });
+            }
+            return list1;
+        }
+
+        public Person GetPerson(int id)
+        {
+            var person = ListPerson().Find(x => x.id_person == id);
+            return person;
+        }
+    }
+}
